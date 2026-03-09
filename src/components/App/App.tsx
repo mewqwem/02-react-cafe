b725cef50a865e4458d55192cb552d97fe4b47fb
Oleh <roadto12mmr@gmail.com>
2026-03-09 20:40:02 +0100

    fix App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import css from "./App.module.css";
-import CafeInfo from "./components/CafeInfo/CafeInfo";
-import { type VoteType } from "./types/votes";
-import type Votes from "./types/votes";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
-import VoteStats from "./components/VoteStats/VoteStats";
-import Notification from "./components/Notification/Notification";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import { type VoteType } from "../../types/votes";
+import type Votes from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 function App() {
   const [votes, setVotes] = useState<Votes>({ good: 0, neutral: 0, bad: 0 });
